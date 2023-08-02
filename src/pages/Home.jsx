87ba41af13e3549d8283b518d3e2ee41a5efaee3
tranthekhiem14112001 <trankhiem99999@gmail.com>
2023-08-02 12:04:27 +0700

    add NavBar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import MusicPlayBar from '../component/MusicPlayBar'
-import Navbar from '../component/Navbar'
+import SideBar from '../component/SideBar'
 import Songspage from '../component/Songspage'
 import { useState } from 'react'
 
@@ -11,7 +11,7 @@ export default function Home() {
   }
   return (
     <div style={{height: "100%", background:"#1a1a1a"}}>
-        <Navbar handleChange={handleChange}/>
+        <SideBar handleChange={handleChange}/>
         <Songspage search={search}/>  
         <MusicPlayBar/>
     </div>
