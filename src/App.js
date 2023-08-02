ecafb63f742ecf33f83f78a8eccc75e31af4dce1
tranthekhiem14112001 <trankhiem99999@gmail.com>
2023-08-02 21:57:27 +0700

    simple list of the song uploaded

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import {Routes, Route, Navigate} from "react-router-dom"
 import Home from './pages/Home';
 import LoginComponent from "./pages/Login";
 import SignupComponent from "./pages/Signup";
+import Songspage from "./component/Songspage";
+import React from "react";
+import TableUnstyled from "./component/SongsUploaded";
 
 
 function App() {
@@ -11,7 +13,10 @@ function App() {
         <div className="App">
 
             <Routes>
-                <Route path='/' element={<Home/>}/>
+                <Route path='/' element={<Home/>}>
+                    <Route path="/" element={<Songspage/>}/>
+                    <Route path="/songs-uploaded" element={<TableUnstyled/>}/>
+                </Route>
                 <Route path="/login" element={<LoginComponent/>}/>
                 <Route path="/signup" element={<SignupComponent/>}/>
                 <Route path="*" element={<Navigate to="/login"/>}/>
