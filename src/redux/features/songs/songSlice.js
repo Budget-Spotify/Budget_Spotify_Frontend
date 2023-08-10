79a865c639f1f855bd9ee8f751f951012d24ea2f
langochai <langochai11121998@gmail.com>
2023-08-11 02:08:25 +0700

    upgrade playbar from one song to a list

--- src/redux/features/songs/songSlice.js
+++ src/redux/features/songs/songSlice.js
@@ -3,14 +3,24 @@ import {createSlice} from "@reduxjs/toolkit";
 export const songSlice = createSlice({
     name: 'song',
     initialState: {
-        song: {}
+        song: {},
+        currentPlaylist: []
     },
     reducers: {
         setSong: (state, action) => {
             state.song = {...action.payload}
+        },
+        setPlayList: (state, action) =>{
+            state.currentPlaylist = [...action.payload]
+        },
+        resetPlayList: (state, action)=>{
+            state.currentPlaylist = []
+        },
+        addSongIntoPlayList: (state, action) =>{
+            state.currentPlaylist.push(action.payload)
         }
     }
 })
 
-export const {setSong} = songSlice.actions;
+export const {setSong, setPlayList, resetPlayList, addSongIntoPlayList} = songSlice.actions;
 export default songSlice.reducer;
