4d177fdc9ced72ec07b8b0f8e4911a30a9ac22c2
KY <nguyenhoangky23121997@gmail.com>
2023-08-10 12:41:15 +0700

    2

--- src/component/Playlist.jsx
+++ src/component/Playlist.jsx
@@ -6,17 +6,17 @@ import {faPlay} from '@fortawesome/free-solid-svg-icons';
 
 
 export default function PlayList() {
-    return (
+  return (
     <div style={{
-                marginLeft: "20.5%",
-                marginBottom: "155px",
-                marginRight: "1%",
-                color: "#fff",
+      marginLeft: "20.5%",
+      marginBottom: "155px",
+      marginRight: "1%",
+      color: "#fff",
       padding: "30px 20px 20px 20px",
-                background: "black",
-                borderRadius: "10px",
+      background: "black",
+      borderRadius: "10px",
   }} >
-
-            </div>
-    );
+    
+    </div>
+  );
 }
