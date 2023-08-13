208c33e4d7f66f5d38d9a6195692b4ed0676be77
Duy Khánh Nguyễn <ndkhanh1996@gmail.com>
2023-08-13 20:30:20 +0700

    complete

--- src/refreshToken/axios-interceptor.js
+++ src/refreshToken/axios-interceptor.js
@@ -5,24 +5,27 @@ export const axiosInstance = axios.create();
 
 axiosInstance.interceptors.request.use(
     async (config) => {
-        const accessToken = localStorage.getItem('token');
-        const refreshToken = localStorage.getItem('refreshToken');
-        console.log(accessToken)
-        console.log(refreshToken)
+        let accessToken = localStorage.getItem('token');
+        let refreshToken = localStorage.getItem('refreshToken');
 
         if (accessToken) {
             const decodedToken = jwtDecode(accessToken);
             const now = Date.now() / 1000;
 
             if (decodedToken.exp < now) {
-                await axios.get("http://localhost:8000/auth/request-refresh-token", {
+               const tokens = await axios.get("http://localhost:8000/auth/request-refresh-token", {
                     headers: {
                         token: `Bearer ${accessToken}`,
                         refreshToken: refreshToken
                     }
                 });
+                await localStorage.setItem("token", tokens.data.accessToken);
+                await localStorage.setItem("refreshToken", tokens.data.refreshToken);
             }
         }
+
+        accessToken = localStorage.getItem('token');
+        refreshToken = localStorage.getItem('refreshToken');
 
         if (accessToken) {
             config.headers.token = `Bearer ${accessToken}`;
