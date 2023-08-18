9147cf7f7defbf46292171ab24cc7fb1574a3ca0
NDKhanh1996 <ndkhanh1996@gmail.com>
2023-08-18 17:30:41 +0700

    change to pc

--- src/component/Notification.jsx
+++ src/component/Notification.jsx
@@ -1,9 +1,11 @@
 import Box from '@mui/material/Box';
 import Popper from '@mui/material/Popper';
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import NotificationsIcon from '@mui/icons-material/Notifications';
 
 export default function Notification() {
+    const userInfo = JSON.parse(localStorage.getItem('userLogin'));
+    const userId = userInfo._id;
     const [anchorEl, setAnchorEl] = useState(null);
 
     const handleClick = (event) => {
@@ -12,6 +14,26 @@ export default function Notification() {
 
     const open = Boolean(anchorEl);
     const id = open ? 'simple-popper' : undefined;
+
+    useEffect(() => {
+        const eventSource = new EventSource(
+            "http://localhost:8000/sse/notifyInNavbar/" + userId
+        );
+
+        eventSource.onmessage = (event) => {
+            const eventData = JSON.parse(event.data);
+            console.log(eventData)
+        };
+
+        eventSource.onerror = (error) => {
+            console.error("SSE Error:", error);
+            eventSource.close();
+        };
+
+        return () => {
+            eventSource.close();
+        };
+    }, []);
 
     return (
         <div>
